a684a09e01d04cd5a2254e37cf02c7012daa1b4d
Tom Chestnut <tchestnut85@gmail.com>
2021-11-30 20:44:29 -0500

    made the SongQueue component and moved dummy data into its own file

--- src/components/SongQueue.js
+++ src/components/SongQueue.js
@@ -1,7 +1,73 @@
 import React from 'react';
+import { Avatar, IconButton, Typography } from '@mui/material';
+import { DUMMY_DATA, PLACEHOLDER_URL } from '../utils/data';
+import { Delete } from '@mui/icons-material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+	avatar: {
+		width: 44,
+		height: 44,
+	},
+	text: {
+		textOverflow: 'ellipsis',
+		overflow: 'hidden',
+	},
+	container: {
+		display: 'grid',
+		gridAutoFlow: 'column',
+		gridTemplateColumns: '50px auto 50px',
+		gridGap: 12,
+		alignItems: 'center',
+		marginTop: 10,
+	},
+	songInfoContainer: {
+		overflow: 'hidden',
+		whiteSpace: 'nowrap',
+	},
+});
+
+const QueuedSong = ({ song }) => {
+	const classes = useStyles();
+	const { artist, title } = song;
+
+	return (
+		<div className={classes.container}>
+			<Avatar
+				src={PLACEHOLDER_URL}
+				alt='Song Thumbnail'
+				className={classes.avatar}
+			/>
+			<div className={classes.songInfoContainer}>
+				<Typography variant='subtitle2' className={classes.text}>
+					{title}
+				</Typography>
+				<Typography
+					color='textSecondary'
+					variant='body2'
+					className={classes.text}
+				>
+					{artist}
+				</Typography>
+			</div>
+			<IconButton>
+				<Delete color='error' />
+			</IconButton>
+		</div>
+	);
+};
 
 const SongQueue = () => {
-	return <div>Song Queue</div>;
+	return (
+		<div style={{ margin: '10px 0' }}>
+			<Typography color='textSecondary' variant='button'>
+				Queue (5)
+			</Typography>
+			{Array.from({ length: 5 }, () => DUMMY_DATA).map((DUMMY_DATA, i) => (
+				<QueuedSong key={i} song={DUMMY_DATA} />
+			))}
+		</div>
+	);
 };
 
 export default SongQueue;
---
+++ src/utils/data.js
@@ -0,0 +1,8 @@
+export const DUMMY_DATA = {
+	title: 'Deceiver of the Gods',
+	artist: 'Amon Amarth',
+	thumbnail: 'https://i1.sndcdn.com/avatars-000002626831-y05l3t-t500x500.jpg',
+};
+
+export const PLACEHOLDER_URL =
+	'https://i1.sndcdn.com/avatars-000002626831-y05l3t-t500x500.jpg';
