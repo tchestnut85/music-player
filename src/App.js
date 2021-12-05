058f4b3df19058559327bceac1fe72d4b5c836b9
Tom Chestnut <tchestnut85@gmail.com>
2021-12-05 15:34:59 -0500

    setup ApolloClient in App.js to use GraphQL

--- src/App.js
+++ src/App.js
@@ -4,50 +4,59 @@ import Header from './components/Header';
 import SongList from './components/SongList';
 import SongPlayer from './components/SongPlayer';
 import { Grid, useMediaQuery, Hidden } from '@mui/material';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const GQL_URI = 'https://music-player-tc.herokuapp.com/v1/graphql';
+const client = new ApolloClient({
+	uri: GQL_URI,
+	cache: new InMemoryCache(),
+});
 
 function App() {
 	const greaterThanMd = useMediaQuery(theme => theme.breakpoints.up('md'));
 	const greaterThanSm = useMediaQuery(theme => theme.breakpoints.up('sm'));
 
 	return (
-		<div>
-			<Hidden only='xs'>
-				<Header />
-			</Hidden>
-			<Grid container spacing={3} style={{ paddingInline: 50 }}>
-				<Grid
-					style={{ paddingTop: greaterThanSm ? 100 : 10 }}
-					item
-					xs={12}
-					md={7}
-				>
-					<AddSong />
-					<SongList />
+		<ApolloProvider client={client}>
+			<div>
+				<Hidden only='xs'>
+					<Header />
+				</Hidden>
+				<Grid container spacing={3} style={{ paddingInline: 50 }}>
+					<Grid
+						style={{ paddingTop: greaterThanSm ? 100 : 10 }}
+						item
+						xs={12}
+						md={7}
+					>
+						<AddSong />
+						<SongList />
+					</Grid>
+					<Grid
+						style={
+							greaterThanMd
+								? {
+										position: 'fixed',
+										width: '100%',
+										right: '2%',
+										top: 70,
+								  }
+								: {
+										position: 'fixed',
+										left: 0,
+										bottom: 0,
+										width: '100%',
+								  }
+						}
+						item
+						xs={12}
+						md={5}
+					>
+						<SongPlayer />
+					</Grid>
 				</Grid>
-				<Grid
-					style={
-						greaterThanMd
-							? {
-									position: 'fixed',
-									width: '100%',
-									right: '2%',
-									top: 70,
-							  }
-							: {
-									position: 'fixed',
-									left: 0,
-									bottom: 0,
-									width: '100%',
-							  }
-					}
-					item
-					xs={12}
-					md={5}
-				>
-					<SongPlayer />
-				</Grid>
-			</Grid>
-		</div>
+			</div>
+		</ApolloProvider>
 	);
 }
 
